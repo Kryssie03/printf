7a94fd73f27529d8d4710e550d5877e3834099fd
Kryssie03 <kosisochukwuakabekwa@gmail.com>
2022-10-17 15:53:42 -0700

    printf

---
+++ main.h
@@ -0,0 +1,38 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define INIT_BITS (4 * 8)
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * struct call - struct call
+ * @t: t - flag for data type
+ * @f: function - associated function
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
+#endif
